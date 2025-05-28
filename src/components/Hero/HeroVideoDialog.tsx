ea5262de8953802439307d4fa2f0c5f5ee309716
sahil1330 <sahilmane025@gmail.com>
2025-05-28 11:02:27 +0530

    fix(HeroVideoDialog): update thumbnail source from .webp to .png format

--- src/components/Hero/HeroVideoDialog.tsx
+++ src/components/Hero/HeroVideoDialog.tsx
@@ -7,14 +7,14 @@ export function HeroVideoDialogComponent() {
         className="block dark:hidden"
         animationStyle="from-center"
         videoSrc="https://www.youtube.com/embed/uCmFiOVW0A8?si=8oONgKedfe0vTWJr"
-        thumbnailSrc="/assets/images/thumbnail.webp"
+        thumbnailSrc="/assets/images/thumbnail.png"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="from-center"
         videoSrc="https://www.youtube.com/embed/uCmFiOVW0A8?si=8oONgKedfe0vTWJr"
-        thumbnailSrc="/assets/images/thumbnail.webp"
+        thumbnailSrc="/assets/images/thumbnail.png"
         thumbnailAlt="Hero Video"
       />
     </div>
