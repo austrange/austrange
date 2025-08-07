ee4fe27d5fc9cab6365f79c44db6e79addb581f2
Sahil Mane <eshkon.sahil@gmail.com>
2025-08-07 17:02:35 +0530

    Update HeroVideoDialog to use a new video source URL

--- src/components/Hero/HeroVideoDialog.tsx
+++ src/components/Hero/HeroVideoDialog.tsx
@@ -6,14 +6,14 @@ export function HeroVideoDialogComponent() {
       <HeroVideoDialog
         className="block dark:hidden"
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/uCmFiOVW0A8?si=8oONgKedfe0vTWJr"
+        videoSrc="https://www.youtube.com/embed/J3ZlFoEV96k?si=MB7FRvIKo4UNKKnU"
         thumbnailSrc="/assets/images/thumbnail.png"
         thumbnailAlt="Hero Video"
       />
       <HeroVideoDialog
         className="hidden dark:block"
         animationStyle="from-center"
-        videoSrc="https://www.youtube.com/embed/uCmFiOVW0A8?si=8oONgKedfe0vTWJr"
+        videoSrc="https://www.youtube.com/embed/J3ZlFoEV96k?si=MB7FRvIKo4UNKKnU"
         thumbnailSrc="/assets/images/thumbnail.png"
         thumbnailAlt="Hero Video"
       />
